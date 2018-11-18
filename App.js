e2da02f6b55cead31c72c3adad755317db42a74b
liuwan950628 <1027968784@qq.com>
2018-11-18 16:59:48 +0800

    second

--- App.js
+++ App.js
@@ -8,17 +8,46 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,Image} from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components'
 
-
+// const props ={}
 export default class App extends Component{
+  constructor(props){
+    super(props);
+    // this.state={
+    //   selectedTab:'home'
+    // }
+  }
   render() {
     return (
+      // <Navigator />
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Navigator
+            />
+         {/* <Navigator /> */}
+        {/* <Navigator /> */}
+          {/* <Navigator.Item></Navigator.Item> */}
+          {/* <Navigator.Item
+            selected={this.state.selectedTab === 'home'}
+            title="Home"
+            renderIcon={() => <Image source={require('./res/image/ic_polular.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/image/ic_polular.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'home' })}>
+            <View style={styles.page1}></View>
+          </Navigator.Item>
+          <Navigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            renderIcon={() => <Image source={require('./res/image/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/image/ic_trending.png')} />}
+            renderBadge={() => <CustomBadgeView />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <View style={styles.page2}></View>
+          </Navigator.Item> */}
+        {/* </Navigator> */}
       </View>
-    );
+    )
   }
 }
 
@@ -27,5 +56,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-
+  page1:{
+    flex:1,
+    backgroundColor: 'red'
+  },
+  page1:{
+    flex:2,
+    backgroundColor: 'yellow'
+  }
 });
--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 
 * 项目记录，第一步，安装组件 
   - npm install react-native-tab-navigator --save
+  * * *
+  该组件有更新
+  应该使用npm install react-native-deprecated-custom-components --save安装
 * 导入第三方组件 
-  - import TabNavigator from 'react-native-tab-navigator'
+  - import TabNavigator from 'react-native-deprecated-custom-components'
 * 使用TabNavigator进行底部导航
 * selectedTab默认home
+#### 2.记录一次报错
+1.ReactNative项目报UnhandledPromiseRejectionWarning 错误
+原因：权限问题在上层目录下输入chmod -R 777 xxx(项目名) 解决
 
